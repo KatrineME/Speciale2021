b8761b51c108c1ec82f14d04c272dd9b79da0e71
michalablicher <s153649@student.dtu.dk>
2021-04-08 16:40:28 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -7,3 +7,21 @@ Created on Thu Apr  8 16:20:42 2021
 """
 
 print("Hello World Lets Go Home")
+import torch
+import os
+#import cv2
+import nibabel as nib
+import numpy   as np
+#import pandas  as pd
+import matplotlib.pyplot as plt
+import torchvision
+import glob2
+#import time
+
+#from skimage.transform import resize
+from torch import nn
+from torch import Tensor
+
+#!pip install torch-summary
+#!pip install opencv-python
+print("NOW!!")
