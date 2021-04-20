a92e28bbd2e9b5471ca8e66dda08dee82c6b7984
michalablicher <59653728+michalablicher@users.noreply.github.com>
2021-04-20 15:00:28 +0200

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,16 +6,16 @@ Created on Thu Apr  8 16:20:42 2021
 @author: michalablicher
 """
 
-print("Hello World Lets Go Home")
+print("Hello World Testing Packages")
 import torch
 import os
 #import cv2
-#import nibabel as nib
+import nibabel as nib
 import numpy   as np
 #import pandas  as pd
 #import matplotlib.pyplot as plt
 import torchvision
-#import glob2
+import glob2
 #import time
 
 #from skimage.transform import resize
@@ -24,4 +24,4 @@ from torch import Tensor
 
 #!pip install torch-summary
 #!pip install opencv-python
-print("NOW!!")
+print("Finish")
