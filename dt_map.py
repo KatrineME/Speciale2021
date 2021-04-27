75055e9b88e9581b53c88f3f417dabf73448998c
KatrineME <59653736+KatrineME@users.noreply.github.com>
2021-04-27 16:19:19 +0200

    Update dt_map.py

--- dt_map.py
+++ dt_map.py
@@ -43,24 +43,29 @@ gt_ed_oh = torch.nn.functional.one_hot(Tensor(gt_ed_flat).to(torch.int64), num_c
 
 dt_es = np.zeros((gt_es_oh.shape))
 dt_ed = np.zeros((gt_ed_oh.shape))
+
+dt = np.zeros((gt_ed_oh.shape))
+
 ref_border_es = np.zeros((gt_es_oh.shape))
 ref_border_ed = np.zeros((gt_ed_oh.shape))
 
 inside_obj_mask_es = np.zeros_like(gt_es_oh).astype(np.bool)
 inside_obj_mask_ed = np.zeros_like(gt_ed_oh).astype(np.bool)
 
-error_margin_inside  = 2 # VOXELS
+error_margin_inside  = 0 # VOXELS
 error_margin_outside = 3 # VOXELS
 
 dt_map = np.zeros((gt_es_oh.shape))
 
-for i in range(0, gt_es_oh.shape[0]):
+#for i in range(0, gt_es_oh.shape[0]):
+for i in range(1, 4):
     for j in range(0, gt_es_oh.shape[3]):
         inside_voxels_indices   = binary_erosion(gt_es_oh[i,:,:,j], iterations=1)
         ref_border_es[i,:,:,j]  = np.logical_xor(gt_es_oh[i,:,:,j], inside_voxels_indices)
-        ref_border_es = ref_border_es.astype(bool)
+        ref_border_es           = ref_border_es.astype(bool)
         
         dt_es[i,:,:,j]          = distance_transform_edt(ref_border_es[i,:,:,j])
+        dt[i,:,:,j]  = dt_es[i,:,:,j] 
         
         inside_obj_mask_es[i,inside_voxels_indices,j] = 1
         
@@ -68,20 +73,21 @@ for i in range(0, gt_es_oh.shape[0]):
         #ref_border_ed[i,:,:,j]  = np.logical_xor(gt_ed_oh[i,:,:,j], inside_voxels_indices)
         #dt_ed[i,:,:,j]          = distance_transform_edt(ref_border_ed)
         
-        #inside_obj_mask_ed[i,inside_voxels_indices,j] = 1
+        
+        inside_obj_mask_ed[i,inside_voxels_indices,j] = 1
         
         
         
-        outside_obj_mask = np.logical_and(inside_obj_mask_es, ref_border_es)
+        #outside_obj_mask = np.logical_and(inside_obj_mask_es, ref_border_es)
         # surface border distance is always ZERO
-        dt_es[ref_border_es] = 0
+        #dt_es[ref_border_es] = 0
         
         # inside structure: we subtract a fixed margin
-        dt_es[inside_obj_mask_es] = dt_es[inside_obj_mask_es] - error_margin_inside
+        dt_es[i,inside_obj_mask_es[i,:,:,j],j] = dt_es[i,inside_obj_mask_es[i,:,:,j],j] - error_margin_inside
         
         # outside of target: structure we subtract a fixed margin.
-        dt_es[outside_obj_mask] = dt_es[outside_obj_mask] - error_margin_outside
-        dt_es[dt_es < 0] = 0
+        #dt_es[outside_obj_mask] = dt_es[outside_obj_mask] - error_margin_outside
+        #dt_es[dt_es < 0] = 0
         
     print(i)
         #dt_map[i,:,:,j] = dt_es[i,:,:,j]
