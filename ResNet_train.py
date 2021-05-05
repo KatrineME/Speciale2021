94454c12a8e715bca04337eb818adcf458a1b9af
KatrineME <59653736+KatrineME@users.noreply.github.com>
2021-05-05 17:10:29 +0200

    Si update

--- ResNet_train.py
+++ ResNet_train.py
@@ -382,7 +382,171 @@ gt_flat_test_es = np.concatenate(data_gt_es[lim_eval:lim_test]).astype(None)
 im_flat_test_ed = np.concatenate(data_im_ed[lim_eval:lim_test]).astype(None)
 gt_flat_test_ed = np.concatenate(data_gt_ed[lim_eval:lim_test]).astype(None)
 
+#%% Load U-NET
+#% BayesUNet
+# recursive implementation of Unet
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv2d') != -1:
+    
+        nn.init.kaiming_normal_(m.weight)
+        m.bias.data.zero_()
+
+
+class UNet(nn.Module):
+    def __init__(self, num_classes=3, in_channels=1, initial_filter_size=64, kernel_size=3, num_downs=4,
+                 norm_layer=nn.InstanceNorm2d, drop_prob=0.):
+        super(UNet, self).__init__()
+        self.drop_prob = drop_prob
+        # construct UNet structure
+        unet_block = UnetSkipConnectionBlock(in_channels=initial_filter_size * 2 ** (num_downs-1), out_channels=initial_filter_size * 2 ** num_downs,
+                                             num_classes=num_classes, kernel_size=kernel_size, norm_layer=norm_layer,
+                                             innermost=True, drop_prob=self.drop_prob)
+        for i in range(1, num_downs):
+            unet_block = UnetSkipConnectionBlock(in_channels=initial_filter_size * 2 ** (num_downs-(i+1)),
+                                                 out_channels=initial_filter_size * 2 ** (num_downs-i),
+                                                 num_classes=num_classes, kernel_size=kernel_size, submodule=unet_block,
+                                                 norm_layer=norm_layer, drop_prob=self.drop_prob
+                                                 )
+            
+        unet_block = UnetSkipConnectionBlock(in_channels=in_channels, out_channels=initial_filter_size,
+                                             num_classes=num_classes, kernel_size=kernel_size, submodule=unet_block, norm_layer=norm_layer,
+                                             outermost=True, drop_prob=self.drop_prob)
+
+        self.model = unet_block
+        self.log_softmax = nn.LogSoftmax(dim=1)
+        self.softmax = nn.Softmax(dim=1)
+        self.apply(weights_init)
+
+    def forward(self, x):
+        out = self.model(x)
+        return {'log_softmax': self.log_softmax(out), 'softmax': self.softmax(out)}
+
+
+# Defines the submodule with skip connection.
+# X -------------------identity---------------------- X
+#   |-- downsampling -- |submodule| -- upsampling --|
+class UnetSkipConnectionBlock(nn.Module):
+    def __init__(self, in_channels=None, out_channels=None, num_classes=1, kernel_size=3,
+                 submodule=None, outermost=False, innermost=False, norm_layer=nn.InstanceNorm2d, drop_prob=0.):
+        super(UnetSkipConnectionBlock, self).__init__()
+        self.use_dropout = True if drop_prob > 0. else False
+        self.drop_prob = drop_prob
+        self.outermost = outermost
+        # downconv
+        pool = nn.MaxPool2d(2, stride=2)
+        conv1 = self.contract(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, norm_layer=norm_layer)
+        conv2 = self.contract(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size, norm_layer=norm_layer)
+
+        # upconv
+        conv3 = self.expand(in_channels=out_channels*2, out_channels=out_channels, kernel_size=kernel_size)
+        conv4 = self.expand(in_channels=out_channels, out_channels=out_channels, kernel_size=kernel_size)
+
+        if outermost:
+            final = nn.Conv2d(out_channels, num_classes, kernel_size=1)
+            if self.use_dropout:
+                down = [conv1, conv2, nn.Dropout2d(self.drop_prob)]
+            else:
+                down = [conv1, conv2]
+            if self.use_dropout:
+                up = [conv3, nn.Dropout2d(self.drop_prob), conv4, nn.Dropout2d(self.drop_prob), final]
+            else:
+                up = [conv3, conv4, final]
+            model = down + [submodule] + up
+        elif innermost:
+            upconv = nn.ConvTranspose2d(in_channels*2, in_channels,
+                                        kernel_size=2, stride=2)
+            model = [pool, conv1, conv2, upconv]
+        else:
+            upconv = nn.ConvTranspose2d(in_channels*2, in_channels, kernel_size=2, stride=2)
+
+            down = [pool, conv1, conv2]
+            up = [conv3, conv4, upconv]
+
+            if self.use_dropout:
+                model = down + [nn.Dropout2d(self.drop_prob)] + [submodule] + up + [nn.Dropout2d(self.drop_prob)]
+            else:
+                model = down + [submodule] + up
+
+        self.model = nn.Sequential(*model)
+
+    @staticmethod
+    def contract(in_channels, out_channels, kernel_size=3, norm_layer=nn.InstanceNorm2d):
+        layer = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=1),
+            norm_layer(out_channels),
+            nn.LeakyReLU(inplace=True))
+        return layer
+
+    @staticmethod
+    def expand(in_channels, out_channels, kernel_size=3):
+        layer = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, padding=1),
+            nn.LeakyReLU(inplace=True),
+        )
+        return layer
+
+    @staticmethod
+    def center_crop(layer, target_width, target_height):
+        batch_size, n_channels, layer_width, layer_height = layer.size()
+
+        xy1 = (layer_width - target_width) // 2
+        xy2 = (layer_height - target_height) // 2
+        return layer[:, :, xy1:(xy1 + target_width), xy2:(xy2 + target_height)]
+
+    def forward(self, x):
+        if self.outermost:
+            out = self.model(x)
+            return out
+        else:
+            crop = self.center_crop(self.model(x), x.size()[2], x.size()[3])
+            out = torch.cat([x, crop], 1)
+            return out
+
+
+class BayesUNet(UNet):
+
+    def __init__(self, num_classes=3, in_channels=1, initial_filter_size=64, kernel_size=3, num_downs=4,
+                 norm_layer=nn.InstanceNorm2d, drop_prob=0.):
+        super(BayesUNet, self).__init__(num_classes, in_channels, initial_filter_size, kernel_size, num_downs,
+                 norm_layer=norm_layer, drop_prob=drop_prob)
+
+    def train(self, mode=True, mc_dropout=False):
+        """ Sets the module in training mode.
+            !!! OVERWRITING STANDARD PYTORCH METHOD for nn.Module
+
+            OR
+                if mc_dropout=True and mode=False (use dropout during inference) we set all modules
+                to train-mode=False except for DROPOUT layers
+                In this case it is important that the module_name matches BayesDRNSeg.dropout_layer
+
+        Returns:
+            Module: self
+        """
+        self.training = mode
+        for module_name, module in self.named_modules():
+            module.training = mode
+            if mc_dropout and not mode:
+                if isinstance(module, nn.Dropout2d):
+                    # print("WARNING - nn.Module.train - {}".format(module_name))
+                    module.training = True
+
+        return self
+
+    def eval(self, mc_dropout=False):
+        """Sets the module in evaluation mode.
+
+        This has any effect only on certain modules. See documentations of
+        particular modules for details of their behaviors in training/evaluation
+        mode, if they are affected, e.g. :class:`Dropout`, :class:`BatchNorm`,
+        etc.
+        """
+        return self.train(False, mc_dropout=mc_dropout)
+
+if __name__ == "__main__":
+    #import torchsummary
+    unet = BayesUNet(num_classes=4, in_channels=1, drop_prob=0.1)
 #%% Load Model
 PATH_model_es = "C:/Users/katrine/Documents/Universitet/Speciale/Trained_Unet_CE_sys_nor20.pt"
 PATH_model_ed = "C:/Users/katrine/Documents/Universitet/Speciale/Trained_Unet_CE_dia_nor_20e.pt"
@@ -482,20 +646,29 @@ optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, eps=1e-04, weight_d
 
 num_epoch = 3
 print('Number of epochs = ',num_epoch)
+#%% Load T_j
+os.chdir("C:/Users/katrine/Documents/GitHub/Speciale2021")
+from SI_error_set_func import SI_set
+
+T_j = SI_set('K', 'dia', lim_eval,lim_test)
+
 
 #%% Prep data
 T = np.expand_dims(T_j, axis=1)
-T = T[0:input_concat.shape[0],:,:]
 
-input_concat_train = input_concat[0:30,:,:,:]
-input_concat_eval = input_concat[30:,:,:,:]
+train_amount = 50
 
-T_train = T[0:30,:,:,:]
-T_eval = T[30:,:,:,:]
+input_concat_train = input_concat[0:train_amount,:,:,:]
+input_concat_eval  = input_concat[train_amount:,:,:,:]
+
+T_train = T[0:train_amount,:,:,:]
+T_eval  = T[train_amount:,:,:,:]
 
 #%% Training
-losses = []
-losses_eval = []
+train_losses = []
+eval_losses  = []
+eval_loss    = 0.0
+train_loss   = 0.0
 
 
 trainloader = input_concat_train
@@ -504,7 +677,6 @@ for epoch in range(num_epoch):  # loop over the dataset multiple times
     
     model.train()
     print('Epoch train =',epoch)
-    train_loss = []
     for i, data in enumerate(trainloader, 0):
         # get the inputs
         #inputs, labels = data
@@ -533,15 +705,16 @@ for epoch in range(num_epoch):  # loop over the dataset multiple times
         loss.backward()
         # Update Weights
         optimizer.step()
-        # Calculate loss
-        train_loss.append(loss.item()) #.detach().cpu().numpy()
         
-    losses.append(np.mean(train_loss)) # This is normalised by batch size
-    train_loss = []
+        # Calculate loss
+        train_loss += loss.item()
+        #train_loss.append(loss.item()) #.detach().cpu().numpy()
+        
+    train_losses.append(train_loss/data.shape[0]) # This is normalised by batch size
+    train_loss = 0.0
 
     model.eval()
-    batch_eval_loss = []
-    for i, data in enumerate(input_concat_eval, 0):
+    for i, data_eval in enumerate(input_concat_eval, 0):
         # get the inputs
         #inputs, labels = data
         inputs = input_concat_eval
@@ -563,17 +736,17 @@ for epoch in range(num_epoch):  # loop over the dataset multiple times
         output = output["log_softmax"]
         
         # Find loss
-        loss_eval = criterion(output, labels)
+        loss = criterion(output, labels)
         
         # Calculate gradients
-        loss_eval.backward()
+        loss.backward()
         # Update Weights
         optimizer.step()
         # Calculate loss
-        batch_eval_loss.append(loss_eval.item())#.detach().cpu().numpy()
+        eval_loss += loss.item()#.detach().cpu().numpy()
         
-    losses_eval.append(np.mean(batch_eval_loss)) # This is normalised by batch size
-    batch_eval_loss = []
+    eval_losses.append(eval_loss/data_eval.shape[0]) # This is normalised by batch size
+    eval_loss = 0.0
     
 print('Finished Training + Evaluation')
 
@@ -582,8 +755,8 @@ epochs = np.arange(len(losses))
 epochs_eval = np.arange(len(losses_eval))
 
 plt.figure(dpi=200)
-plt.plot(epochs + 1 , losses, 'b', label='Training Loss')
-plt.plot(epochs_eval + 1 , losses_eval, 'r', label='Validation Loss')
+plt.plot(epochs + 1 , train_losses, 'b', label='Training Loss')
+plt.plot(epochs_eval + 1 , eval_losses, 'r', label='Validation Loss')
 plt.xticks(np.arange(1,num_epoch+1, step = 1))
 plt.xlabel('Epochs')
 plt.ylabel('Loss')
@@ -598,6 +771,7 @@ output_test = out_test['softmax'].detach().numpy()
 
 image = 30
 
+plt.figure(dpi=200)
 plt.subplot(1,2,1)
 plt.imshow(output_test[image,0,:,:])
 plt.title('Prob. of no seg. failure')
