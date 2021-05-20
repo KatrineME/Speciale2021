cbfb05137945aa80dc03ac78f5989b9652793739
michalablicher <s153649@student.dtu.dk>
2021-05-20 15:47:17 +0200

    Update dataloader_dice.py

--- dataloader_dice.py
+++ dataloader_dice.py
@@ -386,9 +386,7 @@ for epoch in range(num_epoch):  # loop over the dataset multiple times
         inputs = Tensor(np.expand_dims(eval_data[:,0,:,:], axis = 1))
         inputs = inputs.cuda()
         labels = eval_data[:,1,:,:]
-        #labels = torch.nn.functional.one_hot(Tensor(labels).to(torch.int64), num_classes=4)#.detach().numpy()
-        #labels = labels.permute(0,3,1,2)
-        labels = Tensor(labels) #np.expand_dims(labels,axis=1))
+        labels = Tensor(np.expand_dims(labels, axis=1))
         labels = labels.cuda()
         
         #print('i=',i)
