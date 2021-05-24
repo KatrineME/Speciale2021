d685eccfde4478316c6598085253afb4b56705b1
KatrineME <59653736+KatrineME@users.noreply.github.com>
2021-05-24 14:45:04 +0200

    Update load_data_gt_im_sub.py

--- load_data_gt_im_sub.py
+++ load_data_gt_im_sub.py
@@ -92,8 +92,8 @@ def load_data_sub(user, phase, diagnose):
         #centercrop_gt = Tensor(np.zeros((H,W,gt_slices)))
             
         for j in range(0,im_slices):
-            center_img = centercrop(Tensor(img[:,:,j]))
-            centercrop_img[:,:,j]  = center_img
+            centercrop_img[:,:,j] = centercrop(Tensor(img[:,:,j]))
+            #centercrop_img[:,:,j]  = center_img
             #centercrop_img[:,:,j] = (center_img-torch.mean(center_img)) / torch.std(center_img)
             #centercrop_img[:,:,j] = Tensor(cv2.normalize(center_img.detach().numpy(), None, 255, 0, cv2.NORM_MINMAX))   
             
