20cea4e4fd4313358a7e114459937feef1f2b7de
michalablicher <s153649@student.dtu.dk>
2021-05-31 15:06:02 +0200

    Update dataloader_CE_CV.py

--- dataloader_CE_CV.py
+++ dataloader_CE_CV.py
@@ -401,7 +401,7 @@ for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
             # Set total and correct
             predicted = np.argmax(output.detach().cpu().numpy(), axis=1)
             #predicted = Tensor(predicted).cuda()
-            total += Tensor(labels.detach().cpu().numpy()).shape[0]
+            total += (labels.detach().cpu().numpy()).shape[0]
             correct += (predicted == labels).sum().item()
 
       
