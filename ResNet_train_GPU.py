259c45daeb7ae2c76991940dc45e5b5045166dac
KatrineME <59653736+KatrineME@users.noreply.github.com>
2021-06-08 08:56:06 +0200

    Update ResNet_train_GPU.py

--- ResNet_train_GPU.py
+++ ResNet_train_GPU.py
@@ -625,21 +625,21 @@ part = 126 # Memory fail
 
 unet_es.eval()
 
-#out_trained_ed1 = unet_es(Tensor(im_train_es_res[0:part,:,:,:]).cuda())
+out_trained_ed1 = unet_es(Tensor(im_train_es_res[0:part,:,:,:]).cuda())
 #out_trained_ed1 = unet_es(Tensor(im_train_es_res))
-#out_image_es1   = out_trained_ed1["softmax"]
+out_image_es1   = out_trained_ed1["softmax"]
 
 #gt_train_es_res = gt_train_es_res[0:part,:,:]
 #gt_train_es_res = gt_train_es_res
 
-#print('out_image_es1 shape: ', out_image_es1.shape)
+print('out_image_es1 shape: ', out_image_es1.shape)
 
 out_trained_ed2 = unet_es(Tensor(im_train_es_res[part:175,:,:,:]).cuda())
-out_image_es   = out_trained_ed2["softmax"]
+out_image_es2   = out_trained_ed2["softmax"]
 
-#print('out_image_es2 shape: ', out_image_es2.shape)
+print('out_image_es2 shape: ', out_image_es2.shape)
 
-#out_image_es = np.concatenate(out_image_es1, out_image_es2)
+out_image_es = np.concatenate(out_image_es1, out_image_es2)
 
 print('out_image_es shape: ', out_image_es.shape)
 
