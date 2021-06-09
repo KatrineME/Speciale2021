ba4d98e627e35ac82dffe6c28414f42d4fea066e
KatrineME <59653736+KatrineME@users.noreply.github.com>
2021-06-09 10:48:09 +0200

    Update dataloader_CE_CV.py

--- dataloader_CE_CV.py
+++ dataloader_CE_CV.py
@@ -320,7 +320,7 @@ for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
     # Initialize optimizer
     optimizer = torch.optim.Adam(unet.parameters(), lr=0.001, eps=1e-4, weight_decay=1e-4) #LR 
     #lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealing(optimizer, T_max=5)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5)
     
     #% Training
     train_losses  = []
