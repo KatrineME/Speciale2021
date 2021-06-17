185a6bdf4f805f60bfb7c67723ef8b152166f589
michalablicher <s153649@student.dtu.dk>
2021-06-17 20:33:07 +0200

    dice for lc

--- RUN_UNET_analysis_ensemble.py
+++ RUN_UNET_analysis_ensemble.py
@@ -283,7 +283,7 @@ print('Data loaded+concat')
 #%%
 H = 128
 W = 128
-CV_folds = 2
+CV_folds = 6
 data_im = im_test_ed_sub.shape[0]
 
 
@@ -317,7 +317,7 @@ for fold in range(0,6):
 #out_soft_mean = out_soft.mean(axis=0)
 
 if user == 'GPU':
-    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100dia_dice_lclv_new.pt'
+    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100dia_dice_lclv.pt'
 if user == 'K':
     PATH_out_soft = 'C:/Users/katrine/Desktop/Optuna/Out_softmax_fold_avg_200dia_dice_2lclv.pt'
     
