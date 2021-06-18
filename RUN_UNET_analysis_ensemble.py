ade0d6ad5006dce66161109409cfdd1937f2a6e2
michalablicher <s153649@student.dtu.dk>
2021-06-18 13:43:30 +0200

    Update RUN_UNET_analysis_ensemble.py

--- RUN_UNET_analysis_ensemble.py
+++ RUN_UNET_analysis_ensemble.py
@@ -296,7 +296,7 @@ im_data = torch.utils.data.DataLoader(im_test_ed_sub, batch_size=1, shuffle=Fals
 
 for fold in range(0,6):
     if user == 'GPU':
-        path_model ='/home/michala/Speciale2021/Speciale2021/Trained_Unet_dice_lclv_dia_100_fold{}.pt'.format(fold)
+        path_model ='/home/michala/Speciale2021/Speciale2021/Trained_Unet_dice_lc_dia_100_fold{}.pt'.format(fold)
     if user == 'K':
         path_model = 'C:/Users/katrine/Desktop/Optuna/Trained_Unet_dice_2lclv_dia_200_fold{}.pt'.format(fold)
     model = torch.load(path_model, map_location=torch.device(device))
@@ -317,7 +317,7 @@ for fold in range(0,6):
 #out_soft_mean = out_soft.mean(axis=0)
 
 if user == 'GPU':
-    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100dia_dice_lclv.pt'
+    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100dia_dice_lc.pt'
 if user == 'K':
     PATH_out_soft = 'C:/Users/katrine/Desktop/Optuna/Out_softmax_fold_avg_200dia_dice_2lclv.pt'
     
@@ -374,7 +374,7 @@ out_5 = out_5["softmax"].detach().numpy()
 """
 #%% Load model if averagered on GPU
 """
-path_out_soft = '/Users/michalablicher/Desktop/Out_softmax_fold_avg_200dia_dice_lclv_new.pt'
+path_out_soft = '/Users/michalablicher/Desktop/Out_softmax_fold_avg_100dia_dice_lclv.pt'
 #path_out_soft = 'C:/Users/katrine/Desktop/Optuna/Out_softmax_fold_avg_200dia_dice_2lclv.pt'
 
 out_soft = torch.load(path_out_soft ,  map_location=torch.device(device))
@@ -457,7 +457,7 @@ out_seg_mean = torch.nn.functional.one_hot(torch.as_tensor(out_seg_mean_am), num
 
 ref = torch.nn.functional.one_hot(torch.as_tensor(Tensor(gt_test_ed_sub).to(torch.int64)), num_classes=4).detach().cpu().numpy()
 #%%
-test_slice = 264
+test_slice = 314
 plt.figure(dpi=200)
 plt.imshow(out_seg_mean_am[test_slice,:,:])
 #%%
@@ -745,7 +745,15 @@ plt.imshow(seg_dia_oh[slice,:,:,c])
 plt.imshow(seg_sys_oh[slice,:,:,c], alpha =0.5)
 
 b = np.sum(ref_dia_oh[slice,:,:,c])
-"""
+
+
+#%%
+cor_edv = np.corrcoef(target_vol_ed,ref_vol_ed)
+
+
+
+
+
 
 
 
