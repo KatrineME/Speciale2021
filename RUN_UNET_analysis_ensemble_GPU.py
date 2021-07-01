f6a1e6fbc3edfd97ed8735c284ae0c468705b989
michalablicher <s153649@student.dtu.dk>
2021-07-01 12:17:19 +0200

    Update RUN_UNET_analysis_ensemble_GPU.py

--- RUN_UNET_analysis_ensemble_GPU.py
+++ RUN_UNET_analysis_ensemble_GPU.py
@@ -319,7 +319,7 @@ for fold in range(0,6):
             out = model(Tensor(im).cuda())
         else:
             out = model(Tensor(im))
-        out_soft[fold,i,:,:,:] = out["softmax"].detach().cpu().numpy()
+        out_soft[fold,i,:,:,:] = out["log_softmax"].detach().cpu().numpy()
         
         
     del path_model, model, out
@@ -328,7 +328,7 @@ for fold in range(0,6):
 #out_soft_mean = out_soft.mean(axis=0)
 
 if user == 'GPU':
-    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100sys_dice_0lc_2lv.pt'
+    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_100sys_dice_0lc_2lv_log.pt'
 if user == 'K':
     PATH_out_soft = 'C:/Users/katrine/Desktop/Optuna/Out_softmax_fold_avg_200dia_dice_2lclv.pt'
     
