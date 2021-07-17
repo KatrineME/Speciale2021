f2ed6d0337a0eb5a6548e957599b206028abb364
michalablicher <s153649@student.dtu.dk>
2021-07-17 12:51:56 +0200

    Update dt_map_cluster.py

--- dt_map_cluster.py
+++ dt_map_cluster.py
@@ -29,43 +29,58 @@ from torch import Tensor
 
 
 #%% Specify directory
-#os.chdir("C:/Users/katrine/Documents/GitHub/Speciale2021")
-os.chdir('/Users/michalablicher/Documents/GitHub/Speciale2021')
-#os.chdir('C:/Users/katrine/Documents/GitHub/Speciale2021')
-from load_data_gt_im_sub import load_data_sub
-user = 'M'
-#user = 'K' 
-data_im_es_DCM,  data_gt_es_DCM  = load_data_sub(user,'Systole','DCM')
-data_im_es_HCM,  data_gt_es_HCM  = load_data_sub(user,'Systole','HCM')
-data_im_es_MINF, data_gt_es_MINF = load_data_sub(user,'Systole','MINF')
-data_im_es_NOR,  data_gt_es_NOR  = load_data_sub(user,'Systole','NOR')
-data_im_es_RV,   data_gt_es_RV   = load_data_sub(user,'Systole','RV')
+if device == 'cuda':
+    user = 'GPU'
+else:
+    user = 'K'
 
-data_im_ed_DCM,  data_gt_ed_DCM  = load_data_sub(user,'Diastole','DCM')
-data_im_ed_HCM,  data_gt_ed_HCM  = load_data_sub(user,'Diastole','HCM')
-data_im_ed_MINF, data_gt_ed_MINF = load_data_sub(user,'Diastole','MINF')
-data_im_ed_NOR,  data_gt_ed_NOR  = load_data_sub(user,'Diastole','NOR')
-data_im_ed_RV,   data_gt_ed_RV   = load_data_sub(user,'Diastole','RV')
+if user == 'M':
+    os.chdir('/Users/michalablicher/Documents/GitHub/Speciale2021')
+if user == 'K':
+    os.chdir('C:/Users/katrine/Documents/GitHub/Speciale2021')
+if user == 'GPU':
+    os.chdir('/home/michala/Speciale2021/Speciale2021')
+#%%
+user = 'M'
+from load_data_gt_im_sub_space import load_data_sub
+
+phase = 'Systole'
+
+data_im_es_DCM,  data_gt_es_DCM  = load_data_sub(user,phase,'DCM')
+data_im_es_HCM,  data_gt_es_HCM  = load_data_sub(user,phase,'HCM')
+data_im_es_MINF, data_gt_es_MINF = load_data_sub(user,phase,'MINF')
+data_im_es_NOR,  data_gt_es_NOR  = load_data_sub(user,phase,'NOR')
+data_im_es_RV,   data_gt_es_RV   = load_data_sub(user,phase,'RV')
+
+phase = 'Diastole'
+
+data_im_ed_DCM,  data_gt_ed_DCM  = load_data_sub(user,phase,'DCM')
+data_im_ed_HCM,  data_gt_ed_HCM  = load_data_sub(user,phase,'HCM')
+data_im_ed_MINF, data_gt_ed_MINF = load_data_sub(user,phase,'MINF')
+data_im_ed_NOR,  data_gt_ed_NOR  = load_data_sub(user,phase,'NOR')
+data_im_ed_RV,   data_gt_ed_RV   = load_data_sub(user,phase,'RV')
+
 
 #%% BATCH GENERATOR
-num_train_sub = 16 
-num_eval_sub  = num_train_sub + 2
-num_test_sub  = num_eval_sub + 2
+num_train_sub = 12
+num_eval_sub = num_train_sub
+num_test_sub = num_eval_sub + 8
+"""
+im_train_ed_sub = np.concatenate((np.concatenate(data_im_ed_DCM[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_im_ed_HCM[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_im_ed_MINF[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_im_ed_NOR[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_im_ed_RV[0:num_train_sub]).astype(None)))
 
-im_test_es_sub = np.concatenate((np.concatenate(data_im_es_DCM[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_im_es_HCM[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_im_es_MINF[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_im_es_NOR[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_im_es_RV[num_eval_sub:num_test_sub]).astype(None)))
+gt_train_ed_sub = np.concatenate((np.concatenate(data_gt_ed_DCM[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_gt_ed_HCM[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_gt_ed_MINF[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_gt_ed_NOR[0:num_train_sub]).astype(None),
+                                  np.concatenate(data_gt_ed_RV[0:num_train_sub]).astype(None)))
 
-gt_test_es_sub = np.concatenate((np.concatenate(data_gt_es_DCM[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_gt_es_HCM[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_gt_es_MINF[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_gt_es_NOR[num_eval_sub:num_test_sub]).astype(None),
-                                  np.concatenate(data_gt_es_RV[num_eval_sub:num_test_sub]).astype(None)))
-
-
-
+gt_test_ed_sub = gt_train_ed_sub
+im_test_ed_sub = im_train_ed_sub
+"""
 im_test_ed_sub = np.concatenate((np.concatenate(data_im_ed_DCM[num_eval_sub:num_test_sub]).astype(None),
                                   np.concatenate(data_im_ed_HCM[num_eval_sub:num_test_sub]).astype(None),
                                   np.concatenate(data_im_ed_MINF[num_eval_sub:num_test_sub]).astype(None),
@@ -78,40 +93,34 @@ gt_test_ed_sub = np.concatenate((np.concatenate(data_gt_ed_DCM[num_eval_sub:num_
                                   np.concatenate(data_gt_ed_NOR[num_eval_sub:num_test_sub]).astype(None),
                                   np.concatenate(data_gt_ed_RV[num_eval_sub:num_test_sub]).astype(None)))
 
+im_test_es_sub = np.concatenate((np.concatenate(data_im_es_DCM[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_im_es_HCM[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_im_es_MINF[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_im_es_NOR[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_im_es_RV[num_eval_sub:num_test_sub]).astype(None)))
+
+gt_test_es_sub = np.concatenate((np.concatenate(data_gt_es_DCM[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_gt_es_HCM[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_gt_es_MINF[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_gt_es_NOR[num_eval_sub:num_test_sub]).astype(None),
+                                  np.concatenate(data_gt_es_RV[num_eval_sub:num_test_sub]).astype(None)))
+print('Data loaded+concat')
 
 
+#%% Load model if averagered on GPU
 
-#%% Load Model
-#PATH_model = "C:/Users/katrine/Documents/Universitet/Speciale/Trained_Unet_CE_sys_nor20.pt"
-#PATH_state = "C:/Users/katrine/Documents/Universitet/Speciale/Trained_Unet_CE_dia_nor_20e.p"
-PATH_model_ed = '/Users/michalablicher/Desktop/Trained_Unet_CE_dia_sub_batch_100.pt'
-PATH_model_es = '/Users/michalablicher/Desktop/Trained_Unet_CE_sys_sub_batch_100.pt'
+#path_out_soft = '/Users/michalablicher/Desktop/Out_softmax_fold_avg_100dia_dice_lclv.pt'
+#path_out_soft = 'C:/Users/katrine/Desktop/Optuna/Final CV models/Out_softmax_fold_avg_150sys_dice_lclv.pt'
 
-# Load
-unet_es = torch.load(PATH_model_es, map_location=torch.device('cpu'))
-unet_ed = torch.load(PATH_model_ed, map_location=torch.device('cpu'))
-#model.load_state_dict(torch.load(PATH_state))
+out_soft = torch.load(path_out_soft ,  map_location=torch.device(device))
 
-#%% Running  models 
-unet_es.eval()
-out_trained_es = unet_es(Tensor(im_test_es_sub))
-out_image_es    = out_trained_es["softmax"]
-#%%
-unet_ed.eval()
-out_trained_ed = unet_ed(Tensor(im_test_ed_sub))
-out_image_ed    = out_trained_ed["softmax"]
+#%% Mean + argmax + one hot
 
-#%% One hot encoding data
-seg_met_dia = np.argmax(out_image_ed.detach().numpy(), axis=1)
+out_soft_mean   = out_soft.mean(axis=0)
+out_seg_mean_am = np.argmax(out_soft_mean, axis=1)
+out_seg_mean    = torch.nn.functional.one_hot(torch.as_tensor(out_seg_mean_am), num_classes=4).detach().cpu().numpy()
 
-seg_dia = torch.nn.functional.one_hot(torch.as_tensor(seg_met_dia), num_classes=4).detach().numpy()
-ref_dia = torch.nn.functional.one_hot(Tensor(gt_test_ed_sub).to(torch.int64), num_classes=4).detach().numpy()
-
-seg_met_sys = np.argmax(out_image_es.detach().numpy(), axis=1)
-
-seg_sys = torch.nn.functional.one_hot(torch.as_tensor(seg_met_sys), num_classes=4).detach().numpy()
-ref_sys = torch.nn.functional.one_hot(Tensor(gt_test_es_sub).to(torch.int64), num_classes=4).detach().numpy()
-
+ref = torch.nn.functional.one_hot(torch.as_tensor(Tensor(gt_test_es_sub).to(torch.int64)), num_classes=4).detach().cpu().numpy()
 
 #%% Cluster filter
 
