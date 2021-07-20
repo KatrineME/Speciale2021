46aafab59fb404d15c8eb4134930e708f06446ae
michalablicher <s153649@student.dtu.dk>
2021-07-20 13:02:23 +0200

    Update ResNet_train_CV_GPU.py

--- ResNet_train_CV_GPU.py
+++ ResNet_train_CV_GPU.py
@@ -540,8 +540,8 @@ def get_loss(log_pred_probs, lbls, pred_probs=None):
 
     # pred_probs last 2 dimensions need to be merged because lbls has shape [batch_size, w, h ]
     #pred_probs = pred_probs.view(pred_probs.size(0), 2, -1)
-    print('log_pred_probs', log_pred_probs.shape)
-    print('lbls.float()', lbls.float().shape)
+    #print('log_pred_probs', log_pred_probs.shape)
+    #print('lbls.float()', lbls.float().shape)
     fn_soft = (log_pred_probs[:,0,:,:]) * lbls.float()
     
     # fn_nonzero = torch.nonzero(fn_soft.data).size(0)
@@ -677,6 +677,7 @@ for fold, (train_ids, test_ids) in enumerate(kfold.split(input_concat)):
 
             # Find loss
             loss = get_loss(output, labels)
+            print('loss',loss)
             #print('loss = ', loss)
             
             # Calculate gradients
