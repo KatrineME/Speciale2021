0723fe3a8f010abc7df6414a16e6361ba4ba92ef
michalablicher <s153649@student.dtu.dk>
2021-08-06 17:10:47 +0200

    Update RUN_UNET_analysis_ensemble_GPU.py

--- RUN_UNET_analysis_ensemble_GPU.py
+++ RUN_UNET_analysis_ensemble_GPU.py
@@ -295,18 +295,18 @@ print('Data loaded+concat')
 H = 128
 W = 128
 CV_folds = 6
-data_im = im_test_es_sub.shape[0]
+data_im = im_test_ed_sub.shape[0]
 
 out_soft = np.zeros((CV_folds, data_im, 4, H, W))
 
-im_data = torch.utils.data.DataLoader(im_test_es_sub, batch_size=1, shuffle=False, sampler=None,
+im_data = torch.utils.data.DataLoader(im_test_ed_sub, batch_size=1, shuffle=False, sampler=None,
            batch_sampler=None, collate_fn=None,
            pin_memory=False, drop_last=False, timeout=0,
            worker_init_fn=None, prefetch_factor=2, num_workers=0)
 
 for fold in range(0,6):
     if user == 'GPU':
-        path_model ='/home/michala/Speciale2021/Speciale2021/Trained_Unet_dice_lclv_proof_sys_150e_opt_red_fold{}.pt'.format(fold)
+        path_model ='/home/michala/Speciale2021/Speciale2021/Trained_Unet_dice_lclv_proof_dia_150e_opt_red_fold{}.pt'.format(fold)
     if user == 'K':
         path_model = 'C:/Users/katrine/Desktop/Optuna/Trained_Unet_dice_2lclv_dia_200_fold{}.pt'.format(fold)
     model = torch.load(path_model, map_location=torch.device(device))
@@ -327,7 +327,7 @@ for fold in range(0,6):
 #out_soft_mean = out_soft.mean(axis=0)
 
 if user == 'GPU':
-    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_150sys_dice_lclv_proof.pt'
+    PATH_out_soft = '/home/michala/Speciale2021/Speciale2021/Out_softmax_fold_avg_150dia_dice_lclv_proof.pt'
 if user == 'K':
     PATH_out_soft = 'C:/Users/katrine/Desktop/Optuna/Out_softmax_fold_avg_200dia_dice_2lclv.pt'
     
